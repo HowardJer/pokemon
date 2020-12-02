405aa88012b15d65c9bc3f34771d7f0ce7459ff7
Howard <howardjer@gmail.com>
2020-12-01 23:43:47 -0700

    stuck on resetting cookies

--- js/battle.js
+++ js/battle.js
@@ -12,8 +12,11 @@ let cpuHP = Number(cpu.pokemon.hp);
 let cpuAttack = Number(cpu.pokemon.attack);
 let player = game.player;
 let playerName = player.pokemon.name;
-let playerHP = Number(player.pokemon.hp); 
+let playerHP = Number(player.pokemon.hp);
 let playerAttack = Number(player.pokemon.attack);
+
+
+
 
 
 
@@ -32,13 +35,55 @@ document.querySelector(".cpu .name").innerHTML = cpuName;    //gets name of poke
 document.querySelector(".cpu .health").innerHTML = cpuHP;    //gets hp for pokemon cpu
 document.querySelector(".cpu img").src = cpu.pokemon.image;            //gets image for pokemon cpu
 
+// make button clickable
+
+document.querySelector(".game-button").addEventListener("click", function () {
+    document.querySelector(".game-button").innerHTML = "ARE YOU READY TO DO BATTLE?";
 
 
-document.querySelector(".game-button").addEventListener("click", function() {
-    document.querySelector(".game-button").innerHTML = "ARE YOU READY TO DO BATTLE?";     
-    document.querySelector(".cpu .name").style.color = "red"
 
-});
+        // battle sequence
+
+        let battlelog = document.querySelector('.battlelog');
+
+    if (playerHP > 0 && cpuHP > 0) {
+            cpuHP = cpuHP - playerAttack;
+            playerHP = playerHP - cpuAttack;
+            document.querySelector('.player .health').innerHTML = playerHP;
+            document.querySelector('.cpu .health').innerHTML = cpuHP;
+            // battlelog.innerHTML += '<li>' + playerName + ' inflicted ' + playerAttack + ' points of damage, and '
+            //     + cpuName + ' inflicted ' + cpuAttack + ' points of damage.' + '</li>';
+        
+        }else document.querySelector(".battlelog").innerHTML = "Battle Resulted in a Draw! Reset to Start Again";
+
+        
+        
+     
+        
+
+
+
+
+
+
+    });
+    
+    function score(index) {
+        // const randomNumber = Math.floor(Math.random() * pokemon.length);
+        const game = {
+            player: {
+                pokemon: pokemon[index],
+                currentHealth: pokemon[index].hp
+            },
+            cpu: {
+                pokemon: pokemon[randomNumber],
+                currentHealth: pokemon[randomNumber].hp
+            }
+        }
+    
+        Cookies.set("game", JSON.stringify(game));
+    }
+
 
 /*    
     Deal with your "battle sequence" by:
@@ -52,36 +97,21 @@ document.querySelector(".game-button").addEventListener("click", function() {
 */
 
 
-// document.getElementById("gameBtn").addEventListener("click", function() {
-//     document.getElementById("cpuName").style.color = "red";
-    // function battle() {
-    //     if(playerHP > 0 && cpuHP > 0) {
-    //         // attackButton.style.color = "red";
-    //         cpuHP = cpuHP - playerAttack;
-    //         playerHP = playerHP - cpuAttack;
-    //         document.querySelector('.player .health').innerHTML = playerHP;
-    //         document.querySelector('.cpu .health').innerHTML = cpuHP;
-    //         let battlelog = document.querySelector('.battlelog');
-    //         battlelog.innerHTML += '<li>' + playerName + ' inflicted ' + playerAttack + ' points of damage, and ' 
-    //             + cpuName + ' inflicted ' + cpuAttack + ' points of damage.' + '</li>';
-    //         // battle();
-    //     }
-    // }
 
 
-    
-   
-    
-    
-    
-    
-    
-    
-    
-    /*
-        - Use selectors to target and fill in the img, .name, and .health elements on battle.html
-        - Display the .battlelog contents
-        - If the battle is over, present the user with a button to go back to index.html to start a new round
-          and also wipe the cookies. 
-        - Otherwise, present the user with a button to refresh the page and complete the next battle sequence.
-    */
+
+
+
+
+
+
+
+
+
+/*
+    - Use selectors to target and fill in the img, .name, and .health elements on battle.html
+    - Display the .battlelog contents
+    - If the battle is over, present the user with a button to go back to index.html to start a new round
+      and also wipe the cookies.
+    - Otherwise, present the user with a button to refresh the page and complete the next battle sequence.
+*/
